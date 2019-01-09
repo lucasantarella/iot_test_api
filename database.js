9a0516225ed39dc264b5ec55b9150d8f026247fb
Luca Santarella <luca.santarella@gmail.com>
2019-01-08 19:28:15 -0500

    Added optional port

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ let _conn;
 module.exports = {
   getConnection: () => {
     if (_conn === undefined) {
-      _conn = mongoose.createConnection('mongodb://' + process.env.MONGO_USER + ':' + process.env.MONGO_PASS + '@' + process.env.MONGO_HOST,
+      _conn = mongoose.createConnection('mongodb://' + process.env.MONGO_USER + ':' + process.env.MONGO_PASS + '@' + process.env.MONGO_HOST + (process.env.MONGO_PORT || 27017),
         {
           keepAlive: true,
           keepAliveInitialDelay: 300000,
